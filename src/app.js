70f4592277a04363872ab03040eae20b616a6f34
waka <alexey.sauliak@releaze.dk>
2017-03-07 17:41:30 +0200

    initial setup

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import regexLib from './utils/regexLib';
+
+console.log(regexLib.isUrl);
---
+++ src/utils/regexLib.js
@@ -0,0 +1,5 @@
+const regexLib = {
+    isUrl: /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/
+};
+
+export default regexLib;
